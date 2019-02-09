bee578b20462b890774bd1b2ac88b619931633b9
Vinicius Ferreira <viniciusferreira@localhost.localdomain>
2019-02-09 17:01:35 -0500

    Inclusao de Banco de Dados H2. 200

--- src/main/java/com/product/manager/resources/CategoriaResource.java
+++ src/main/java/com/product/manager/resources/CategoriaResource.java
@@ -1,5 +1,6 @@
 package com.product.manager.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import com.product.manager.services.CategoriasService;
 @RequestMapping("/categorias")
 public class CategoriaResource {
 	
+	@Autowired
 	private CategoriasService categoriasService;
 	
 	@GetMapping("/{id}")
