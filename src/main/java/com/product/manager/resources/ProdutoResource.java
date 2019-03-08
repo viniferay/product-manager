c484f5d5ebabd82ccd30af9a211e6f909b743975
Vinicius Ferreira <viniciusferreira@localhost.localdomain>
2019-03-08 10:18:26 -0300

    Arruma end point Get ID do Produto

--- src/main/java/com/product/manager/resources/ProdutoResource.java
+++ src/main/java/com/product/manager/resources/ProdutoResource.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.product.manager.domain.Produto;
@@ -15,11 +16,11 @@ public class ProdutoResource {
 
 	@Autowired
 	private ProdutoService produtoService;
-	
-	@GetMapping
-	public ResponseEntity<?> fin(Integer id){
+
+	@GetMapping("/{id}")
+	public ResponseEntity<?> fin(@PathVariable Integer id){
 		Produto obj = produtoService.buscarProduto(id);
-		
+
 		return ResponseEntity.ok().body(obj);
 	}
 }
